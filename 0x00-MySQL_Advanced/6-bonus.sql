4770e4dd36c69b4e98f2a9fb29831707f176db0a
emmanueladdo <asememmanueladdo@gmail.com>
2023-12-15 01:47:32 +0000

    bonus

--- 0x00-MySQL_Advanced/6-bonus.sql
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -1,8 +1,5 @@
--- write SQL script that creates a stored procedure AddBonus that adds a new correction for a student
--- Procedure AddBonus is taking 3 inputs (in this order):
--- user_id, a users.id value (you can assume user_id is linked to an existing users)
---cproject_name, a new or already exists projects - if no projects.name found in the table, you should create it
--- score, the score value for the correction
+-- SQL script that creates a stored procedure AddBonus
+-- that adds a new correction for a student.
 DELIMITER $$ ;
 CREATE PROCEDURE AddBonus(
         IN user_id INTEGER,
