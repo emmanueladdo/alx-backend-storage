1be0299ea6a93d9a401102ae411dd9ce262312eb
emmanueladdo <asememmanueladdo@gmail.com>
2023-12-17 00:54:52 +0000

    weighted average

--- 0x00-MySQL_Advanced/100-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/100-average_weighted_score.sql
@@ -1,6 +1,5 @@
---Write a SQL script that creates a 
---stored procedure ComputeAverageWeightedScoreForUser 
---that computes and store the average weighted score for a student.
+-- Average weighted score - creates a stored procedure ComputeAverageWeightedScoreForUser that computes and store the average weighted score for a student.
+-- a users.id value (you can assume user_id is linked to an existing users)
 DROP PROCEDURE IF EXISTS ComputeAverageWeightedScoreForUser;
 DELIMITER $$
 CREATE PROCEDURE ComputeAverageWeightedScoreForUser(user_id INT)
