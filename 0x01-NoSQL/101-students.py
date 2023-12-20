3cf49c35e258f1c04ebabe0a0091eff80788b63d
emmanueladdo <asememmanueladd@gmail.com>
2023-12-20 00:37:43 +0000

    students

--- 0x01-NoSQL/101-students.py
+++ 0x01-NoSQL/101-students.py
@@ -5,30 +5,16 @@ Python function that returns all students sorted by average score:
 
 
 def top_students(mongo_collection):
-    """
-    Aggregate pipeline to calculate the average score for each student
-    """
-    pipeline = [
+    """ Returns all students sorted by average score """
+
+    topcls = mongo_collection.aggregate([
         {
-            '$unwind': '$topics'
-        },
-        {
-            '$group': {
-                '_id': '$_id',
-                'name': {'$first': '$name'},
-                'averageScore': {'$avg': '$topics.score'}
+            "$project": {
+                "name": "$name",
+                "averageScore": {"$avg": "$topics.score"}
             }
         },
-        {
-            '$sort': {'averageScore': -1}
-        }
-    ]
+        {"$sort": {"averageScore": -1}}
+    ])
 
-    # Execute the aggregation pipeline
-    result = list(mongo_collection.aggregate(pipeline))
-
-    # Add the averageScore to each item in the result
-    for item in result:
-        item['averageScore'] = round(item['averageScore'], 2)
-
-    return result
+    return topcls
